0a0e1de00904b3e7519fa34e076e6a6cdaa1dc92
Leonard Rumeghea <79212407+LeonardRumeghea@users.noreply.github.com>
2022-11-27 14:25:29 +0200

    Clear

---
+++ VetAppointment.API/Dtos/PetDto.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+namespace VetAppointment.API.Dtos
+{
+    public class PetDto
+    {
+        public string Name { get; set; }
+
+        public string Birthdate { get; set; }
+
+        public string Race { get; set; }
+
+        public string Gender { get; set; }
+
+    }
+}

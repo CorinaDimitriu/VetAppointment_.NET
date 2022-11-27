cdb24827cfefb7588c08904663f29f10ed72f1c6
Leonard Rumeghea <79212407+LeonardRumeghea@users.noreply.github.com>
2022-11-27 16:56:21 +0200

    Clear

--- VetAppointment.API/Dtos/PetOwnerDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using VetAppointment.API.Dtos.Create;
-
-namespace VetAppointment.API.Dtos
-{
-    public class PetOwnerDto : CreatePetOwnerDto
-    {
-        public Guid Id { get; set; }
-    }
-}
--- VetAppointment.Business.Test/Usings.cs
+++
@@ -1,5 +0,0 @@
-global using Xunit;
-global using FluentAssertions;
-global using VetAppointment.Domain;
-global using VetAppointment.Domain.Enums;
-global using VetAppointment.Domain.Helpers;

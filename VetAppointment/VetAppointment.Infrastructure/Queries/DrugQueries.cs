815366ef785bcbc312b27f64118a0c95aa5cda53
Tomihai15 <tomihai15@yahoo.com>
2023-01-08 18:47:17 +0200

    Drug Dapper
    Co-Authored-By: AndreeaJinga <79217722+AndreeaJinga@users.noreply.github.com>

---
+++ VetAppointment/VetAppointment.Infrastructure/Queries/DrugQueries.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace VetAppointment.Infrastructure.Queries
+{
+    [ExcludeFromCodeCoverage]
+    public static class DrugQueries
+    {
+        public static string AllDrugs => "SELECT * FROM [Drug] (NOLOCK)";
+
+        public static string DrugById => "SELECT * FROM [Drug] WHERE [Id]=@Id";
+
+        public static string AddDrug =>
+            @"INSERT INTO [Drug] ([Name],[Quantity], [UnitPrice]) 
+                        VALUES (@Name, @Quantity, @UnitPrice)";
+
+        public static string UpdateDrug =>
+            @"UPDATE [Drug]
+              SET [Name] = @Name,
+                  [Quantity] = @Quantity,
+                  [UnitPrice] = @UnitPrice
+             WHERE [Id] = @Id";
+
+        public static string DeleteDrug => "DELETE FROM [Drug] WHERE [Id] = @Id";
+    }
+}
---
+++ VetAppointment/VetAppointment.Infrastructure/Repositories/GenericRepositories/DrugRepositoryDapper.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.SqlClient;
+using VetAppointment.Domain;
+using VetAppointment.Infrastructure.Data;
+using VetAppointment.Infrastructure.Queries;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+
+namespace VetAppointment.Infrastructure.Repositories.GenericRepositories
+{
+    public class DrugRepositoryDapper : Repository<Drug>
+    {
+        private DatabaseContext _databaseContext;
+        public DrugRepositoryDapper(DatabaseContext context) : base(context)
+        {
+            _databaseContext = context;
+        }
+
+        public override async Task<Drug> Add(Drug entity)
+        {
+            using (IDbConnection connection = new SqlConnection(_databaseContext.Database.GetConnectionString()))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(DrugQueries.AddDrug, entity);
+                return entity;
+            }
+        }
+
+        public override async Task<Drug> Update(Drug entity)
+        {
+            _databaseContext.Update(entity);
+            await _databaseContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public override async Task<Drug> Get(Guid id)
+        {
+            using (IDbConnection connection = new SqlConnection(_databaseContext.Database.GetConnectionString()))
+            {
+                connection.Open();
+                var result = await connection.QuerySingleOrDefaultAsync<Drug>(DrugQueries.DrugById, new { Id = id });
+                return result;
+            }
+        }
+
+        public override async Task Delete(Drug entity)
+        {
+            using (IDbConnection connection = new SqlConnection(_databaseContext.Database.GetConnectionString()))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(DrugQueries.UpdateDrug, new { Id = entity.Id });
+            }
+        }
+
+        public override async Task<IEnumerable<Drug>> All()
+        {
+            using (IDbConnection connection = new SqlConnection(_databaseContext.Database.GetConnectionString()))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Drug>(DrugQueries.AllDrugs);
+                return result.ToList();
+            }
+        }
+    }
+}
